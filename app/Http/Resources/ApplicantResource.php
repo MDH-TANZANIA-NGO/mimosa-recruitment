cdfa60277540cebac2ead5fcba1180a5017d866d
Hamis Juma <hamisjuma1@gmail.com>
2022-06-29 12:47:21 +0300

    modify user

--- app/Http/Resources/ApplicantResource.php
+++ app/Http/Resources/ApplicantResource.php
@@ -21,10 +21,11 @@ class ApplicantResource extends JsonResource
             'first_name' => $this->first_name,
             'middle_name' => $this->middle_name,
             'last_name' => $this->last_name,
+            'email' => $this->user->email,
             'dob' => $this->dob,
             'phone' => $this->phone,
-            'country' => Country::where('id',$this->country_id)->pluck('name'),
-            'gender' => CodeValue::where('id',$this->gender_cv_id)->pluck('name'),
+            'country' => Country::where('id',$this->country_id)->first()->name,
+            'gender' => CodeValue::where('id',$this->gender_cv_id)->first()->name,
             'national' => $this->national_id
         ];
     }
--- app/Models/Applicant/Traits/ApplicantRelationship.php
+++ app/Models/Applicant/Traits/ApplicantRelationship.php
@@ -2,7 +2,12 @@
 
 namespace App\Models\Applicant\Traits;
 
+use App\Models\Auth\User;
+
 trait ApplicantRelationship
 {
-
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
