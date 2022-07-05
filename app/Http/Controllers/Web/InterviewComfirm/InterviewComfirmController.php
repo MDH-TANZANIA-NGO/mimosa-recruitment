6665bd8d2f81710231129213f32b4b949b8bc193
William Mussa <eng.williammussa>
2022-07-05 12:10:28 +0300

    fix bugs

--- app/Http/Controllers/Web/InterviewComfirm/InterviewComfirmController.php
+++ app/Http/Controllers/Web/InterviewComfirm/InterviewComfirmController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Web\InterviewComfirm;
 
 use App\Http\Controllers\Controller;
+use App\Models\Applicant\Applicant;
 use Illuminate\Http\Request;
 
 class InterviewComfirmController extends Controller
 {
     public function index()
     {
-        return view('interviewcomfirm.comfirm');
+        $applicant = Applicant::where('user_id', access()->id())->first();
+        return view('interviewcomfirm.comfirm')
+            ->with('applicant', $applicant);
     }
 }
