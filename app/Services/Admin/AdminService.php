0e14c923405df343cafd16c91194cb2f67fd1755
William Mussa <eng.williammussa>
2022-07-12 14:57:00 +0300

    rectifications

---
+++ app/Services/Admin/AdminService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\Admin;
+
+trait AdminService
+{
+
+    public function getAllApplicantsByJobId($jobId){
+
+    }
+
+    public function getAllJobs(){
+
+    }
+
+    public function getAllApplicantsWithMoreThanOneJob(){
+
+    }
+
+
+}
---
+++ routes/web/admin/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'Admin', 'middleware' => ['web', 'auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('', 'AdminController@index')->name('index');
+    Route::post('store', 'AdminController@store')->name('store');
+    Route::put('{applicant}/update', 'AdminController@update')->name('update');
+    Route::get('preview', 'AdminController@preview')->name('preview');
+
+});
