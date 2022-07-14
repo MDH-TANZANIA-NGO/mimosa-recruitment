0aa3b8e836a552d8551fc36a6fed55356d07ab72
William Mussa <eng.williammussa>
2022-07-14 18:25:18 +0300

    rectifications

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('applications/{hire_requisition_job_id}/applicants', 'Api\Applications\ApplicationController@index')->name('index');
 
 Route::get('applicant/{id}/show', 'Api\Applicant\ApplicantController@show')->name('show');
+Route::get('applicants/{id}/applicant', 'Api\Applicant\ApplicantController@applicant')->name('applicant');
 Route::get('applicant/{id}/resource/{hire_requisition_job_id}', 'Api\Applicant\ApplicantController@resource')->name('resource');
 Route::post('applicant/shortlisted', 'Api\Applicant\ApplicantController@shortlistApplicant')->name('applicant_shortlist');
 Route::post('applicant/unshortlist', 'Api\Applicant\ApplicantController@unshortlistApplicant')->name('applicant_unshortlist');
